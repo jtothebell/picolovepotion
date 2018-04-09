5ef81d3456a88a48e6d54a657e1de4227d4b5148
gamax92 <gamax92@aol.com>
2018-04-09 16:43:34 -0600

    Add support for binary literals

--- cart.lua
+++ cart.lua
@@ -396,6 +396,19 @@ function cart.load_p8(filename)
 	end)
 	-- rewrite assignment operators
 	lua=lua:gsub("(%S+)%s*([%+-%*/%%])=", "%1 = %1 %2 ")
+	-- convert binary literals to hex literals
+	lua=lua:gsub("([^%w_])0[bB]([01.]+)", function(a, b)
+		local p1, p2=b, ""
+		if b:find('.', nil, true) then
+			p1, p2=b:match("(.-)%.(.*)")
+		end
+		-- pad to 4 characters
+		p2=p2..string.rep("0", 3-((#p2-1)%4))
+		p1, p2=tonumber(p1, 2), tonumber(p2, 2)
+		if p1 and p2 then
+			return string.format("%s0x%x.%x", a, p1, p2)
+		end
+	end)
 
 	local cart_env={}
 	for k, v in pairs(api) do
