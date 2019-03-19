08f41ba75a3f48f031af50d5fcfa32355c7f2680
jtothebell <j.to.the.bell@gmail.com>
2019-03-18 21:36:56 -0600

    Set up temp canvases in load()

--- main.lua
+++ main.lua
@@ -77,7 +77,74 @@ function del(a, dv)
 	end
 end
 
+function btn(i, p)
+	if i~=nil or p~=nil then
+		p=p or 0
+		if p<0 or p>1 then
+			return false
+		end
+		return not not pico8.keypressed[p][i]
+	else
+		local bits=0
+		for i=0, 5 do
+			bits=bits+(pico8.keypressed[0][i] and 2^i or 0)
+			bits=bits+(pico8.keypressed[1][i] and 2^(i+8) or 0)
+		end
+		return bits
+	end
+end
+
+--require("strict")
+--local bit=require("bit")
+
+--local flr, abs=math.floor, math.abs
+
+local frametime=1/pico8.fps
+local cart=nil
+local cartname=nil
+local love_args=nil
+--local scale=nil
+--local xpadding=nil
+--local ypadding=nil
+local scale=2
+local xpadding=0
+local ypadding=0
+
+local tobase=nil
+local topad=nil
+local gif_recording=nil
+local gif_canvas=nil
+local osc
+local host_time=0
+local retro_mode=false
+local paused=false
+local mobile=false
+local api, cart, gif
+
+local __buffer_count=8
+local __buffer_size=1024
+local __sample_rate=22050
+local channels=1
+local bits=16
+
+--log=print
+--log=function() end
+
+function updateStatus(newPart)
+	status = status .. '\n' .. newPart
+end
+
+function setColor(c)
+	love.graphics.setColor(c/15, 0, 0, 1)
+end
+
+local exts={'', '.p8'}
+
+
 function love.load()
+	status = 'start of load'
+	updateStatus('updated')
+
 	currentButton =
     {
         pressed = 'None',
@@ -89,21 +156,32 @@ function love.load()
     if OS[2] == "3DS" then
         down = "start"
     end
-    exitKey = down
+	exitKey = down
+	
+	--getDimensions not implemented yet
+	--love.resize(love.graphics.getDimensions()) -- Setup initial scaling and padding
+
+	love.graphics.clear()
+	love.graphics.setDefaultFilter('nearest', 'nearest')
+	pico8.screen=love.graphics.newCanvas(pico8.resolution[1], pico8.resolution[2])
+	pico8.tmpscr=love.graphics.newCanvas(pico8.resolution[1], pico8.resolution[2])
 end
 
 function love.update(dt)
-
+	pico8.frames=pico8.frames+1
+	update_buttons()
 end
 
 function love.draw()
-	love.graphics.print("Hello World!", 10, 10)
+	love.graphics.print("Hello World!!", 310, 10)
 	
 	local i = 0
     for k, v in pairs(currentButton) do
-        love.graphics.print(k .. ": " .. v, 100, 100 + (i * 18))
+        love.graphics.print(k .. ": " .. v, 400, 100 + (i * 18))
         i = i + 1
-       end
+	end
+	
+	love.graphics.print(status, 500, 10)
 end
 
 function love.gamepadpressed(joy, button)
@@ -118,4 +196,40 @@ end
 function love.gamepadreleased(joy, button)
     currentButton.released = button
 	del(currentButtonDown, button)
+end
+
+function update_buttons()
+	local init, loop=pico8.fps/2, pico8.fps/7.5
+	
+	for p=0, 1 do
+		local keymap=pico8.keymap[p]
+		local keypressed=pico8.keypressed[p]
+		for i=0, 5 do
+			local btn=false
+			for _, testkey in pairs(keymap[i]) do
+				if love.keyboard and love.keyboard.isDown then
+					if love.keyboard.isDown(testkey) then
+						btn=true
+						break
+					end
+				elseif currentButtonDown then
+					for _, btnDwn in pairs(currentButtonDown) do
+						if btnDwn == testkey then
+							btn = true
+						end
+					end
+				end
+			end
+			if not btn then
+				keypressed[i]=false
+			elseif not keypressed[i] then
+				pico8.keypressed.counter=init
+				keypressed[i]=true
+			end
+		end
+	end
+	pico8.keypressed.counter=pico8.keypressed.counter-1
+	if pico8.keypressed.counter<=0 then
+		pico8.keypressed.counter=loop
+	end
 end
