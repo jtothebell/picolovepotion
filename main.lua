05b3de41e723029603c47aff8d525ab06351ad83
jtothebell <j.to.the.bell@gmail.com>
2019-03-18 21:08:14 -0600

    Re-add global pico8 object

--- main.lua
+++ main.lua
@@ -1,3 +1,67 @@
+pico8={
+	fps=30,
+	frames=0,
+	pal_transparent={},
+	resolution={128, 128},
+	palette={
+		{0,  0,  0,  255},
+		{29, 43, 83, 255},
+		{126,37, 83, 255},
+		{0,  135,81, 255},
+		{171,82, 54, 255},
+		{95, 87, 79, 255},
+		{194,195,199,255},
+		{255,241,232,255},
+		{255,0,  77, 255},
+		{255,163,0,  255},
+		{255,240,36, 255},
+		{0,  231,86, 255},
+		{41, 173,255,255},
+		{131,118,156,255},
+		{255,119,168,255},
+		{255,204,170,255}
+	},
+	spriteflags={},
+	audio_channels={},
+	sfx={},
+	music={},
+	current_music=nil,
+	usermemory={},
+	cartdata={},
+	clipboard="",
+	keypressed={
+		[0]={},
+		[1]={},
+		counter=0
+	},
+	kbdbuffer={},
+	keymap={
+		[0]={
+			[0]={'left', 'dpleft'},
+			[1]={'right', 'dpright'},
+			[2]={'up', 'dpup'},
+			[3]={'down', 'dpdown'},
+			[4]={'z', 'b'},
+			[5]={'x', 'a'},
+        },
+        [1]={
+			[0]={'s'},
+			[1]={'f'},
+			[2]={'e'},
+			[3]={'d'},
+			[4]={'tab'},
+			[5]={'q'},
+		}
+	},
+	mwheel=0,
+	cursor={0, 0},
+	camera_x=0,
+	camera_y=0,
+	draw_palette={},
+	display_palette={},
+	pal_transparent={},
+}
+
 function add(a, v)
 	if a==nil then return end
 	a[#a+1]=v
