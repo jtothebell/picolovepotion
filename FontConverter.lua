1cb574aa3503eed161d8621a5cd224523e242ead
jtothebell <j.to.the.bell@gmail.com>
2019-05-31 22:19:31 -0600

    Add fontSpriteTable and generating code for ref

--- FontConverter.lua
+++ FontConverter.lua
@@ -2,16 +2,51 @@ function writeFontToText()
 
     local data=love.image.newImageData("font32bit.png")
 
+	local glyphs=""
+	for i=32, 127 do
+		glyphs=glyphs..string.char(i)
+	end
+	for i=128, 153 do
+		glyphs=glyphs..string.char(194, i)
+	end
+	pico8.glyphs = glyphs
+
 	local width,height = data:getDimensions()
-    local fontStr = ''
+    
     local x = 0
-    local y = 0
-    while x < width do
-        
+	local y = 0
+	local charWidth = 3
+
+	local fontTable = {}
+
+	for i = 1, #glyphs do
+		local char = string.sub(glyphs, i, i)
+		fontTable[char] = {}
+		local charX = (i - 1) * (charWidth + 1) + 1
+		for y=0, height - 1 do
+			fontTable[char][y + 1] = {}
+			for x=0, charWidth - 1 do
+				r, g, b, a = data:getPixel(charX + x, y)
+				if a == 0 then
+					fontTable[char][y + 1][x + 1] = 0
+				else 
+					fontTable[char][y + 1][x + 1] = 1
+				end
+			end
+
+		end
+
+	end
+
+	--'http://github.com/kikito/inspect.lua'
+	--local inspect = require 'inspect'
+
+	--love.filesystem.write("fontSpriteSheetTable.txt", inspect(fontTable))
+
+	--print(inspect(fontTable))
 
 
-    end
-
+	local fontStr = ''
 	for y=0, height - 1 do
 		for x=0, width - 1 do
 			r, g, b, a = data:getPixel(x, y)
@@ -26,6 +61,6 @@ function writeFontToText()
 		fontStr = fontStr .. '\n'
 	end
 
-    love.filesystem.write("fontSpriteSheet.txt",fontStr)
+    --love.filesystem.write("fontSpriteSheet.txt",fontStr)
     
 end
